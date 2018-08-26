f5e17701d4ecb54f4d9007187868c20492dc5003
Jorge Reus <j-g1996@live.com>
2018-08-26 18:04:51 -0500

    Finished pitts script

--- pracs/1/code/pitts.m
+++ pracs/1/code/pitts.m
@@ -1,3 +1,29 @@
+% Datos proporcionados por el usuario
+gate = input('Ingrese la compuerta (and, or, not): ', 's');
+syn_prompt = 'Ingrese el valor de los pesos sinápticos separados por espacios(e.g. 1 2 3 4): ';
+w = str2num(strip(input(syn_prompt, 's')));
+theta = input('Ingrese el valor del umbral: ');
+if (gate == "not" && size(w, 2) > 1)
+    fprintf("Error, la compuerta NOT es de una sola entrada");
+else
+    % Generación de la tabla de entradas y targets
+    model = logicalModel(size(w, 2), gate)
+    error = false;
+    % Iteración
+    for i = 1:size(model, 1)
+        row = model(i, :);
+        % Obtención de n
+        n = sum(row(1:end-1).*w);
+        % Obtención de a
+        if(n > theta); a_n = 1; else; a_n=0; end
+        % Comparación de a con el target
+        fprintf("n_%i = %i -> t_%i = %i\n", i, a_n, i, row(end));
+        if(a_n ~= row(end)); error = true; break; end
+    end
+    if(~error); fprintf("El aprendizaje fue exitoso\n"); else; fprintf("El aprendizaje no fue exitoso\n"); end
+end
+
+
 function [table] = logicalModel(i, gate)
     % logicalModel(I, gate) returns a matrix representing a truth table and
     % the last column represents the oupot base on all the previous columns
@@ -39,7 +65,12 @@ function [table] = logicalModel(i, gate)
                 res = res | row(e_index);
             end
             table(row_index, end) = res; 
-        end  
-    end 
-    
+        end 
+    elseif (gate == "not")
+        for row_index = 1:size(table, 1)
+            row = table(row_index,:);
+            res = ~row(1);     
+            table(row_index, end) = res; 
+        end
+    end  
 end
