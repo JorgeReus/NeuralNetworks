6c9403f0a137ac516975907f1b4961af1d4c4b69
Jorge Reus <j-g1996@live.com>
2018-12-04 23:33:46 -0600

    Up to date

--- pracs/5/MLP/resultados/pol3/Polinomio3/MLP/get_activation_function.m
+++
@@ -1,9 +0,0 @@
-function a = get_activation_function(n, config)
-    if (config == 1)
-        a = purelin(n);
-    elseif (config == 2)
-        a = logsig(n);
-    else
-        a = tansig(n);
-    end
-end
